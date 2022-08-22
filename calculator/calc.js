74c036db215094dc5b0aa2156fef5b2ab945a493
P3TR0z <52086073+P3TR0z@users.noreply.github.com>
2022-08-22 22:27:40 +0300

    Optimised calc.js | Implemented degree filtering
    calc.js changes:
      - costValue este acum un text input
      - countries - sortat pe continente
      - optimizat addCountries()
      - optimizat (sters) verifyData, acum
        submitData
    Mai multe detalii pe dis daca vr
    
    programe.js changes:
      - adaugat functia filterPrograms,
        magia se intampla aici
      - functia showPrograms va afisa
        programele frumos cu htmlcss
        SOON :TM:

--- calculator/calc.js
+++ calculator/calc.js
@@ -2,189 +2,175 @@
 const range = document.getElementById('cost-range');
 const costValue = document.querySelector('.range-value');
 
-const europeGrid = document.querySelector('[europe]');
-const americaGrid = document.querySelector('[america]');
-const oceaniaGrid = document.querySelector('[oceania]');
-
 const RON_multiplier = 4.91;
 const USD_multiplier = 1.03;
 
 const dormsCheck = document.getElementById('dorms');
 const submit = document.querySelector('.submit');
 
+let countriesListByContinent;
+let selectedCountry;
 let dormsCost;
-costValue.textContent = range.value;
+costValue.placeholder = range.value;
 range.oninput = function() 
 {
-  costValue.textContent = this.value;
+  costValue.value = this.value;
 }
 range.addEventListener('mousemove',  () =>
 {
-
+    let percent = range.value;
+    let clr = `linear-gradient(to right, lightgray ${percent / 300}%, white ${percent / 300}%)`;
+    range.style.background = clr;
+})
+costValue.addEventListener('input', () => 
+{
+    range.value = costValue.value;    
     let percent = range.value;
     let clr = `linear-gradient(to right, lightgray ${percent / 300}%, white ${percent / 300}%)`;
     range.style.background = clr;
 })
 
-var countries =
-[
-    {
-        "name": "Olanda",
-        "continent": "Europe",
-        "costs":
-        {
-           "min": 2500,
-           "max": 1500
-        },
-        "dorms": 1500
-    },
-    {
-        "name": "Olanda",
-        "continent": "Europe",
-        "costs":
-        {
-           "min": 2500,
-           "max": 15000
-        },
-        "dorms": 1500
-    },
-    {
-        "name": "Olanda",
-        "continent": "Europe",
-        "costs":
-        {
-           "min": 2500,
-           "max": 15000
-        },
-        "dorms": 1500
-    },
-    {
-        "name": "Olanda",
-        "continent": "Europe",
-        "costs":
-        {
-           "min": 2500,
-           "max": 15000
-        },
-        "dorms": 1500
-    },
-    {
-        "name": "Germania",
-        "continent": "Europe",
-        "costs":
-        {
-           "min": 3500,
-           "max": 18000
-        },
-        "dorms": 2200
-    },
-    {
-        "name": "Germania",
-        "continent": "Europe",
-        "costs":
-        {
-           "min": 3500,
-           "max": 18000
-        },
-        "dorms": 2200
-    },
-    {
-        "name": "Germania",
-        "continent": "Europe",
-        "costs":
-        {
-           "min": 3500,
-           "max": 18000
-        },
-        "dorms": 2200
-    },
-    {
-        "name": "Germania",
-        "continent": "Europe",
-        "costs":
-        {
-           "min": 3500,
-           "max": 18000
-        },
-        "dorms": 2200
-    },
-    {
-        "name": "Canada",
-        "continent": "America",
-        "costs":
-        {
-           "min": 4500,
-           "max": 20500
-        },
-        "dorms": 2700
-    },
-    
-    {
-        "name": "Australia",
-        "continent": "Oceania",
-        "costs":
-        {
-           "min": 3800,
-           "max": 15500
-        },
-        "dorms": 2100
-    }
-] 
 async function addCountries()
 {
-    for (let i = 0; i < countries.length; i++)
-    {
-        let country = document.createElement('div');
-        country.classList.add('country');
-        let countryName = document.createElement('h3');
-        countryName.textContent = countries[i].name;
-        if (countries[i].continent === 'Europe')
-            europeGrid.querySelector('.countries').appendChild(country);
-        if (countries[i].continent === 'America')
-            americaGrid.querySelector('.countries').appendChild(country);      
-        if (countries[i].continent === 'Oceania')
-            oceaniaGrid.querySelector('.countries').appendChild(country);
-        country.appendChild(countryName);
-    
-        country.addEventListener('click', ()=>
+
+    let queryList = ['[europe]', '[america]', '[oceania]']; // nu putem avea mai mult de 7 continente deeci vom hard coda pozitia lor in vector
+    for (let i = 0; i < countriesListByContinent.length; i++)
+        for (let j = 0; j < countriesListByContinent[i].countries.length; j++)
         {
-            dormsCheck.checked = false;
-            document.querySelectorAll('.country').forEach((countryItem) =>
-            {
-                countryItem.classList.remove('active')
+            console.log(countriesListByContinent[i].countries[j].name)
+            let country = document.createElement('div');
+            country.classList.add('country');
+            let countryName = document.createElement('h3');
+            countryName.textContent = countriesListByContinent[i].countries[j].name;
+            document.querySelector(queryList[i]).querySelector('.countries').appendChild(country);
+        
+            country.appendChild(countryName);
+    
+            country.addEventListener('click', () => {
+                dormsCheck.checked = false;
+
+                document.querySelectorAll('.country').forEach((countryItem) => // ineficient dar csf. Mai bine tine minte ultimul countryItem setat dar mi-e lene 
+                {
+                    countryItem.classList.remove('active')
+                })
+
+                selectedCountry = countriesListByContinent[i].countries[j];
+                country.classList.add('active');
             })
-            country.classList.add('active');
-        })
-    }
+        }
 }
 window.onload = addCountries;
 
-function verifyData()
+function submitData()
 {
-    for (let i = 0; i < countries.length; i++)
-    {
-        document.querySelectorAll('.country').forEach((country) =>
-        {
-            if (!(country.classList.contains('active')))
-                 return;
-            if ((country.textContent != countries[i].name))
-                 return;
-            
-            submit.href = `./countries/${countries[i].name.toLowerCase()}.html`
-            dormsCost = countries[i].dorms;
-            localStorage.setItem('cost', range.value);
-            localStorage.setItem('min', countries[i].costs.min);
-            localStorage.setItem('max', countries[i].costs.max);
-            console.log(localStorage.getItem('cost'));
+    if (selectedCountry == null)
+        return;
 
-            if(dormsCheck.checked)
-                localStorage.setItem('dorms', dormsCost);
-            else
-                localStorage.setItem('dorms', 0);     
-        
-        })
-    }
+    submit.href = `./countries/${selectedCountry.name.toLowerCase()}.html`
+    dormsCost = selectedCountry.dorms;
+    localStorage.setItem('cost', costValue.value);
+    localStorage.setItem('min', selectedCountry.costs.min);
+    localStorage.setItem('max', selectedCountry.costs.max);
+    console.log(localStorage.getItem('cost'));
+
+    if(dormsCheck.checked)
+        localStorage.setItem('dorms', dormsCost);
+    else
+        localStorage.setItem('dorms', 0);     
 }
+submit.addEventListener('click', submitData);
 
-submit.addEventListener('click', verifyData);
-
+countriesListByContinent = 
+[
+    {
+        "continent": "Europe",
+        "countries":
+        [
+            {
+                "name": "Olanda",
+                "costs":
+                {
+                   "min": 2500,
+                   "max": 1500
+                },
+                "dorms": 1500
+            },
+            {
+                "name": "Olanda",
+                "costs":
+                {
+                   "min": 2500,
+                   "max": 15000
+                },
+                "dorms": 1500
+            },
+            {
+                "name": "Olanda",
+                "costs":
+                {
+                   "min": 2500,
+                   "max": 15000
+                },
+                "dorms": 1500
+            },
+            {
+                "name": "Olanda",
+                "costs":
+                {
+                   "min": 2500,
+                   "max": 15000
+                },
+                "dorms": 1500
+            },
+            {
+                "name": "Germania",
+                "costs":
+                {
+                   "min": 3500,
+                   "max": 18000
+                },
+                "dorms": 2200
+            },
+            {
+                "name": "Germania",
+                "costs":
+                {
+                   "min": 3500,
+                   "max": 18000
+                },
+                "dorms": 2200
+            },
+        ]
+    },
+    {
+        "continent": "America",
+        "countries": 
+        [
+            {
+                "name": "Canada",
+                "costs":
+                {
+                   "min": 4500,
+                   "max": 20500
+                },
+                "dorms": 2700
+            },
+        ]
+    },
+    {
+        "continent": "Ocenia",
+        "countries": 
+        [
+            {
+                "name": "Australia",
+                "continent": "Oceania",
+                "costs":
+                {
+                   "min": 3800,
+                   "max": 15500
+                },
+                "dorms": 2100
+            }
+        ]
+    }
+]
--- programe/programe.js
+++ programe/programe.js
@@ -5,6 +5,10 @@ const subjectResults = subjectContainer.querySelectorAll('.subject-card');
 
 const filterBtn = document.querySelector('.filter-btn');
 
+let selectedSubjects = [];
+
+let degrees; // scope-ul variabilelor globale e pe intreg site-ul / toate sursele -_-. Probabil ca nu-i ok si trebuie sa gasim alt cv.
+
 subjectOptions.forEach((subject) =>
 {
   subject.addEventListener('click', () =>
@@ -26,42 +30,40 @@ subjectOptions.forEach((subject) =>
     card.appendChild(p);
     card.appendChild(btn);
     
-    subject.setAttribute('selected', 'true'); 
-
+    subject.setAttribute('selected', 'true');   
+    selectedSubjects.push(subject.querySelector('.subject-card p').textContent.replace('â', 'a').replace('ă', 'a')); // uite ce ma pui sa fac caline
+    console.log(selectedSubjects);
     checkOptionNumber();
 
-     document.querySelectorAll('.x-button').forEach((xBtn) =>
-      {
-         xBtn.addEventListener('click', () =>
-        {
-            let result = xBtn.parentElement
-            result.parentElement.removeChild(result);
-            checkOptionNumber();
-            subject.setAttribute('selected', 'false');
-        })
-})
+    document.querySelectorAll('.x-button').forEach((xBtn) =>
+    {
+    xBtn.addEventListener('click', () =>
+    {
+        let result = xBtn.parentElement
+        result.parentElement.removeChild(result); // aici da o eroare, nu e de la mine, kktul merge oricum tho
+        checkOptionNumber();
+        subject.setAttribute('selected', 'false');
+        const index = selectedSubjects.indexOf(subject.querySelector('.subject-card p').textContent.replace('â', 'a').replace('ă', 'a'));
+        selectedSubjects.splice(index, 1);
+    })
+    })
   })
 })
 
 function checkOptionNumber()
 {
-  let optionCount = subjectContainer.children.length;
+    let optionCount = subjectContainer.children.length;
     if (optionCount > 0)
-     {
-      document.querySelector('.aid-text').setAttribute('style', 'display: none');
-      
-      console.log(optionCount);
-     }
-     else document.querySelector('.aid-text').setAttribute('style', 'display: block');
+    {
+        document.querySelector('.aid-text').setAttribute('style', 'display: none');
+        console.log(optionCount);
+    }
+    else document.querySelector('.aid-text').setAttribute('style', 'display: block');
 
-     if (optionCount === 3)
-     {
-       document.querySelectorAll('[selected="false"]').forEach((unclicked) => unclicked.classList.add('inactive'));
-     }
-     else
-     {
-      document.querySelectorAll('[selected="false"]').forEach((unclicked) => unclicked.classList.remove('inactive'));
-     }
+    if (optionCount === 3)
+        document.querySelectorAll('[selected="false"]').forEach((unclicked) => unclicked.classList.add('inactive'));
+    else
+        document.querySelectorAll('[selected="false"]').forEach((unclicked) => unclicked.classList.remove('inactive'));
 }
 
 filterBtn.addEventListener('click', () =>
@@ -74,11 +76,154 @@ filterBtn.addEventListener('click', () =>
     return;
   }
 
+  filterPrograms();
   document.querySelector('.error-text').setAttribute('style', 'display: none');
   document.querySelector('.succes-text').setAttribute('style', 'display: block');
-        
 })
 
+function showPrograms(filteredDegrees)
+{
+    filteredDegrees.sort((a, b) => a.score > b.score ? -1 : 1); // sort dupa "score";
+    filteredDegrees.forEach(degree => console.log(degree));
+}
+
+/** "When I wrote this code, only God and I knew how it worked. Now only God knows."/s */
+function filterPrograms()
+{
+    if (selectedSubjects.length == 0)
+        return;
+    
+    // ok deci va parcurge fiecare program din lista de programe <degrees> si va compara tagurile sale <subjects> cu materiile selectate <selectedSubjects>.
+    // Daca gaseste o materie data egala cu tagul programului, adauga programul in arrayul de programe relevante <filteredDegrees>, si ii seteaza "score" la 1 (daca exista deja, score++).
+    // Afisarea se va face pe <filteredDegrees>, sortat dupa "score" crescator. Astfel, programele relevante sunt afisate primele.
+    var filteredDegrees = [];
+    degrees.forEach(degree =>
+    {
+        selectedSubjects.forEach(subject =>
+        {
+            if (degree.subjects.includes(subject))
+            {
+                let found = false;
+                for (let i = 0; i < filteredDegrees.length; i++)
+                {
+                    if (filteredDegrees[i].name == degree.name)
+                    {
+                        filteredDegrees[i].score += 1;
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found)
+                    filteredDegrees.push({ "name": degree.name, "score": 1 });
+            }
+        })
+    })
+    showPrograms(filteredDegrees);
+    filteredDegrees.length = 0; // filteredDegrees.clear() - pentru ca avem functie .includes dar nu avem .clear; ms js
+}
+
+degrees = [
+    {
+        "name": "Computer Science",
+        "subjects": ["Matematica", "Informatica"]
+    },
+    {
+        "name": "Software and Engineer",
+        "subjects": ["Matematica", "Informatica"]
+    },
+    {
+        "name": "Game Dev",
+        "subjects": ["Informatica", "Fizica"]
+    },
+    {
+        "name": "Contabilitate",
+        "subjects": ["Matematica", "Economie"]
+    },
+    {
+        "name": "Arhitectura si Constructii",
+        "subjects": ["Matematica", "Fizica,", "Desen"]
+    },
+    {
+        "name": "Inginerie Aerospatiala",
+        "subjects": ["Matematica", "Fizica"]
+    },
+    {
+        "name": "Arta si Design",
+        "subjects": ["Desen", "Istorie"]
+    },
+    {
+        "name": "Business si Managemenet",
+        "subjects": ["Psihologie, Economie"]
+    },    
+    {
+        "name": "Inginerie Chimica",
+        "subjects": ["Chimie"]
+    },
+    {
+        "name": "Criminologie",
+        "subjects": ["Chimie", "Biologie"]
+    },
+    {
+        "name": "Inginerie Electrica",
+        "subjects": ["Fizica"]
+    },
+    {
+        "name": "Finante",
+        "subjects": ["Economie", "Matematica"]
+    },
+    {
+        "name": "Cinematografie",
+        "subjects": ["Istorie", "Desen", "Engleza"]
+    },
+    {
+        "name": "Jurnalism",
+        "subjects": ["Engleza", "Istorie", "Psihologie"]
+    },
+    {
+        "name": "Drept",
+        "subjects": ["Istorie", "Psihologie"]
+    },
+    {
+        "name": "Marketing",
+        "subjects": ["Desen", "Economie", "Psihologie"]
+    },
+    {
+        "name": "Inginerie Mecanica",
+        "subjects": ["Matematica", "Fizica"]
+    },
+    {
+        "name": "Medicina",
+        "subjects": ["Biologie", "Chimie"]
+    },
+    {
+        "name": "Biofizica",
+        "subjects": ["Biologie", "Fizica"] // btw nu cred ca asta-i biofizica chief 
+    },
+    {
+        "name": "Biochimie",
+        "subjects": ["Biologie", "Chimie"]
+    },
+    {
+        "name": "Biologie Moleculara",
+        "subjects": ["Biologie"]
+    },
+    {
+        "name": "Medicina Veterinara",
+        "subjects": ["Biologie", "Chimie"]
+    },
+    {
+        "name": "Psihologie",
+        "subjects": ["Psihologie"]
+    },
+    {
+        "name": "Politica",
+        "subjects": ["Istorie", "Psihologie"]
+    },
+    {
+        "name": "Mc. Donald's",
+        "subjects": ["Desen", "Istorie", "Psihologie"] // sau noi astia de pe info (nu o sa gasim job)
+    },
+] 
 
 
 //progress bar + slider, in versiunea actuala nu exista
